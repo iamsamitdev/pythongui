9229371a5ee27a8c8806fd7503645061ac109d6c
Sourcery AI <>
2023-08-03 08:12:08 +0000

    'Refactored by Sourcery'

--- gui01.py
+++ gui01.py
@@ -14,8 +14,7 @@ screen_height = mainFrm.winfo_screenheight()
 x_cordinate = int((screen_width/2) - (window_width/2))
 y_cordinate = int((screen_height/2) - (window_height/2))
 
-mainFrm.geometry("{}x{}+{}+{}".format(window_width,
-                                      window_height, x_cordinate, y_cordinate))
+mainFrm.geometry(f"{window_width}x{window_height}+{x_cordinate}+{y_cordinate}")
 
 
 mainFrm.mainloop()  # คำสั่งแสดงผลหน้า GUI
